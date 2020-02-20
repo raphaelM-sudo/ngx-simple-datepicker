0e812e031affa4e0ae2097f55cd7c9c9203eb49a
BuildTools <unconfigured@null.spigotmc.org>
2020-02-20 21:39:41 +0100

    Exported Months model.

--- projects/simple-datepicker/src/public-api.ts
+++ projects/simple-datepicker/src/public-api.ts
@@ -4,4 +4,5 @@
 
 export * from './lib/simple-datepicker.module';
 export { DatepickerComponent } from './lib/components/datepicker/datepicker.component';
+export { Months } from './lib/models/months.model';
 export { SimpleSelectModule } from '@nutrify/ngx-simple-select';
